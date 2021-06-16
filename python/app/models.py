ceb3a7a2c60613b3d7713954493db7ca3459de2f
ehzan <enyazdi@gmail.com>
2021-06-16 07:36:17 +0430

    POST

--- python/app/models.py
+++ python/app/models.py
@@ -1,12 +1,25 @@
+from django import contrib
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+
+
 class Role(models.Model):
-    name=models.CharField(max_length=25)
-    persianName=models.CharField(max_length=25, null=True)
-    description=models.CharField(max_length=255, null=True)
-    hidden=models.BooleanField(default=False)
-    default=models.BooleanField(default=False)
-    checked=models.BooleanField(default=False)
+    name = models.CharField(max_length=30)
+    persianName = models.CharField(max_length=25, null=True)
+    description = models.CharField(max_length=255, null=True)
+    hidden = models.BooleanField(default=False)
+    default = models.BooleanField(default=False)
+    checked = models.BooleanField(default=False)
+
     def __str__(self):
         return self.name
+
+
+class Token(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    token = models.CharField(max_length=255)
+
+    def __str__(self) -> str:
+        return '{}_token'.format(self.user)
--- python/app/views.py
+++ python/app/views.py
@@ -1,3 +1,24 @@
+from json.encoder import JSONEncoder
 from django.shortcuts import render
-
+from django.http import JsonResponse
+from django.views.decorators.csrf import csrf_exempt
+from app.models import User, Role, Token
+from json import JSONEncoder  # ?
 # Create your views here.
+
+
+@csrf_exempt
+def say_hi(request):
+    """ alaki """
+
+    # #TODO: validate data on request/role
+    this_token = request.POST['token']
+    this_user = User.objects.filter(token__token=this_token)
+    this_role = Role.objects.filter(name=request.POST['rolename'])
+
+    print("alakiiiiiiiiiiiiiiiiiiiiiiii")
+    print(this_role)
+
+    return JsonResponse(
+        {'status': '{} fetched'.format(this_role)},
+        JSONEncoder)
