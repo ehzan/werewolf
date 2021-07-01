d3b451197965e8e28391bdb56c0d62b20d3594eb
ehzan <enyazdi@gmail.com>
2021-07-01 17:50:59 +0430

    framework changed to Django

--- python/app/static/main.js
+++ python/app/static/main.js
@@ -55,35 +55,6 @@ function setDefaults() {
     fck_Armored.checked &&= (n_players % 3 != 2);
 }
 
-function go() {
-    const n_players = NumberOfPlayers.value;
-    const n_mafiosi = Math.floor(n_players / 3);
-    const n_citizens = n_players - n_mafiosi;
-    role_map.forEach(role =>
-        role['selected'] = document.getElementById("fck_" + role.name).checked);
-
-    let Players = [];
-    let i_citizens = 0, i_mafiosi = 0;
-    for (let role of role_map.values())
-        if (role.selected && role.team == 'w' && i_citizens < n_citizens) {
-            Players.push({ 'role': role.name, 'key': Math.random() });
-            i_citizens++;
-        }
-        else if (role.selected && role.team == 'b' && i_mafiosi < n_mafiosi) {
-            Players.push({ 'role': role.name, 'key': Math.random() });
-            i_mafiosi++;
-        }
-    while (i_citizens++ < n_citizens)
-        Players.push({ 'role': 'Citizen', 'key': Math.random() });
-    while (i_mafiosi++ < n_mafiosi)
-        Players.push({ 'role': 'Mafioso', 'key': Math.random() });
-
-    Players.sort((a, b) => a.key - b.key);
-    p1.innerText = "";
-    Players.forEach((item, index) =>
-        p1.innerText += `${(index + 1)}. ${item.role} - ${role_map.get(item.role).persianName}\n`);
-}
-
 function show_hide_roles() {
     if (link.innerText == "More Roles") {
         link.innerText = "Less Roles";
